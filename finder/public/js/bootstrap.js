b600de5421057dc038c90e73cf14fb1ecc1364da
minsOne <cancoffee7+github@gmail.com>
2013-06-01 02:23:38 +0900

    modify

--- finder/public/js/bootstrap.js
+++ finder/public/js/bootstrap.js
@@ -293,7 +293,7 @@
     this.options = options
     this.options.pause == 'hover' && this.$element
       .on('mouseenter', $.proxy(this.pause, this))
-      .on('mouseleave', $.proxy(this.cycle, this))
+      .on('mouseleave', $.proxy(this.pause, this))
   }
 
   Carousel.prototype = {
